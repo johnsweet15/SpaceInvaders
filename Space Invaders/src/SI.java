99d2b1b05ed02211d657f273c3709a4913395181
John Sweet <john.sweet.15@cnu.edu>
2017-04-10 20:50:39 -0400

    commit

--- Space Invaders/src/SI.java
+++ Space Invaders/src/SI.java
@@ -1,16 +1,210 @@
-import java.awt.BorderLayout;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.Box;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 
 public class SI extends JFrame{
+    
+    private JMenuBar bar;
+    private JMenu gameMenu;
+    private JMenuItem newGame;
+    private JMenuItem quit;
+    private JMenuItem about;
+    private JMenuItem pause;
+    private JMenuItem resume;
+    private JMenu help;
     
     public SI() {
         super("Space Invaders");
         
-        setLayout(new BorderLayout());
-        pack();
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+//        SIPanel panel = new SIPanel(this);
+//        this.add(panel);
+//        
+//        bar = new JMenuBar();
+//        setJMenuBar(bar);
+//
+//        gameMenu = new JMenu("Game");
+//        help = new JMenu("Help");
+//        
+//        bar.add(gameMenu);
+//        bar.add(Box.createHorizontalGlue());
+//        bar.add(help);
+//        
+//        newGameButton();
+//        pauseButton();
+//        resumeButton();
+//        quitButton();
+//        aboutButton();
+        
+        
+        
+        this.setSize(500, 450);
+//        this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+//        this.setResizable(false);
+//        this.setLocationRelativeTo(null);
+//        
+//        checkCloseWindow();
+        
+        
+    }
+    
+    private void newGameButton() {
+        newGame = new JMenuItem("New Game");
+        gameMenu.add(newGame);
+        gameMenu.addSeparator();
+        newGame.addActionListener(new ActionListener() {
+            /**
+             * Method called when about action occurs
+             */
+            public void actionPerformed(ActionEvent event) {
+                int result = JOptionPane.showConfirmDialog(null,
+                        "Start a new game?", "Confirm",
+                        JOptionPane.YES_NO_OPTION);
+                if (result == JOptionPane.YES_OPTION) {
+                    
+                }
+            }
+        });
+    }
+    
+    private void pauseButton() {
+        pause = new JMenuItem("Pause");
+        gameMenu.add(pause);
+        pause.setEnabled(false);
+        pause.addActionListener(new ActionListener() {
+            /**
+             * Method called when about action occurs
+             */
+            public void actionPerformed(ActionEvent event) {
+                
+            }
+        });
+    }
+    
+    private void resumeButton() {
+        resume = new JMenuItem("Resume");
+        gameMenu.add(resume);
+        resume.setEnabled(false);
+        resume.addActionListener(new ActionListener() {
+            /**
+             * Method called when about action occurs
+             */
+            public void actionPerformed(ActionEvent event) {
+                
+            }
+        });
+    }
+    
+    private void quitButton() {
+        quit = new JMenuItem("Quit");
+        gameMenu.addSeparator();
+        gameMenu.add(quit);
+        quit.addActionListener(new ActionListener() {
+            /**
+             * Method called when about exit occurs
+             */
+            public void actionPerformed(ActionEvent event) {
+                int result = JOptionPane.showConfirmDialog(null,
+                        "Dare to Quit?", "Confirm",
+                        JOptionPane.YES_NO_OPTION);
+                if (result == JOptionPane.YES_OPTION) {
+                    dispose();
+                }
+            }
+        });
+    }
+    
+    private void aboutButton() {
+        about = new JMenuItem("About...");
+        help.add(about);
+        about.addActionListener(new ActionListener() {
+            /**
+             * Method called when about action occurs
+             */
+            public void actionPerformed(ActionEvent event) {
+                JOptionPane.showMessageDialog(null,
+                        "SpaceInvaders\nby John Sweet", "About...",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+    }
+    
+    private void checkCloseWindow() {
+        addWindowListener(new WindowListener() {
+            /**
+             * Method called when window close action occurs
+             */
+            public void windowClosing(WindowEvent arg0) {
+                int result = JOptionPane.showConfirmDialog(null,
+                        "Dare to Quit?", "Confirm",
+                        JOptionPane.YES_NO_OPTION);
+                if (result == JOptionPane.YES_OPTION) {
+                    dispose();
+                }
+            }
+
+            /**
+             * unused method that had to be implemented
+             */
+            public void windowActivated(WindowEvent arg0) {
+            }
+
+            /**
+             * unused method that had to be implemented
+             */
+            public void windowClosed(WindowEvent arg0) {
+            }
+
+            /**
+             * unused method that had to be implemented
+             */
+            public void windowDeactivated(WindowEvent arg0) {
+            }
+
+            /**
+             * unused method that had to be implemented
+             */
+            public void windowDeiconified(WindowEvent arg0) {
+            }
+
+            /**
+             * unused method that had to be implemented
+             */
+            public void windowIconified(WindowEvent arg0) {
+            }
+
+            /**
+             * unused method that had to be implemented
+             */
+            public void windowOpened(WindowEvent arg0) {
+            }
+        });
+    }
+    
+    /**
+     * Runs the frame
+     * @param args
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            /**
+             * Runs the frame
+             */
+            public void run() {
+                JFrame f = new SI();
+                f.setVisible(true);
+            }
+        });
     }
 
 }
--- Space Invaders/src/SIPanel.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JFrame;
-
-public class SIPanel extends JFrame{
-    
-    public SIPanel() {
-        
-    }
-
-}
